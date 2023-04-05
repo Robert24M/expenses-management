d24097038a3663c72e920d2ccb00ee57b76c18a6
Robert <mirob1179@gmail.com>
2023-04-05 19:14:48 +0300

    Project structure

---
+++ src/main/java/we/sisuca/expensesmanagement/ExpensesManagementApplication.java
@@ -0,0 +1,13 @@
+package we.sisuca.expensesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensesManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/we/sisuca/expensesmanagement/ExpensesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package we.sisuca.expensesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
